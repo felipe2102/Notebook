01067753710a0c2fb7e0b0345314db0a13f149c7
Felipe Ribeiro <felipinhooliveira06@gmail.com>
2021-09-27 21:56:14 -0300

    versões antigas

---
+++ old versions/1.0/code.py
@@ -0,0 +1,47 @@
+
+
+aulaseg: str = 'que aulas eu tenho segunda'
+aulater: str = 'que aulas eu tenho terça'
+aulaquar: str = 'que aulas eu tenho quarta'
+aulaquin: str = 'que aulas eu tenho quinta'
+aulasex: str = 'que aulas eu tenho sexta'
+
+pergunta1: str = input('Hi,how can i help?')
+print('artes,ciências,história,algebra e geometria' if pergunta1 == aulaseg else '')
+print('história,inglês,geometria,redaçâo e geografia' if pergunta1 == aulater else '')
+print('ciências,gramática 2x,algebra e educação física' if pergunta1 == aulaquar else '')
+print('geografia,algebra,geografia,inglês e ciências' if pergunta1 == aulaquin else '')
+print('gramática,educaçâo fisíca,inglês,algebra e história' if pergunta1 == aulasex else '')
+
+pergunta2: str = input('')
+print('artes,ciências,história,algebra e geometria' if pergunta2 == aulaseg else '')
+print('história,inglês,geometria,redaçâo e geografia' if pergunta2 == aulater else '')
+print('ciências,gramática 2x,algebra e educação física' if pergunta2 == aulaquar else '')
+print('geografia,algebra,geografia,inglês e ciências' if pergunta2 == aulaquin else '')
+print('gramática,educaçâo fisíca,inglês,algebra e história' if pergunta2 == aulasex else '')
+
+pergunta3: str = input('')
+print('artes,ciências,história,algebra e geometria' if pergunta3 == aulaseg else '')
+print('história,inglês,geometria,redaçâo e geografia' if pergunta3 == aulater else '')
+print('ciências,gramática 2x,algebra e educação física' if pergunta3 == aulaquar else '')
+print('geografia,algebra,geografia,inglês e ciências' if pergunta3 == aulaquin else '')
+print('gramática,educaçâo fisíca,inglês,algebra e história' if pergunta3 == aulasex else '')
+
+pergunta4: str = input('')
+print('artes,ciências,história,algebra e geometria' if pergunta4 == aulaseg else '')
+print('história,inglês,geometria,redaçâo e geografia' if pergunta4 == aulater else '')
+print('ciências,gramática 2x,algebra e educação física' if pergunta4 == aulaquar else '')
+print('geografia,algebra,geografia,inglês e ciências' if pergunta4 == aulaquin else '')
+print('gramática,educaçâo fisíca,inglês,algebra e história' if pergunta4 == aulasex else '')
+
+pergunta5: str = input('')
+print(
+    'artes,ciências,história,algebra e geometria' if pergunta5 == aulaseg else '')  # exit(print('have a good day!') ))
+print(
+    'história,inglês,geometria,redaçâo e geografia' if pergunta5 == aulater else '')  # exit(print('have a good day!') ))
+print(
+    'ciências,gramática 2x,algebra e educação física' if pergunta5 == aulaquar else '')  # exit(print('have a good day!') ))
+print(
+    'geografia,algebra,geografia,inglês e ciências' if pergunta5 == aulaquin else '')  # exit(print('have a good day!') ))
+print(
+    'gramática,educaçâo fisíca,inglês,algebra e história' if pergunta5 == aulasex else '')  # exit(print('have a good day!') ))
